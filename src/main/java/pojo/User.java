c6ce49734c4a123c4502d547b59c464b19ae1d69
lyx <liangyuxin.02@gmail.com>
2016-12-28 19:25:22 +0800

    modify

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,13 @@
+package pojo;
+
+/**
+ * Created by uc on 2016/12/28.
+ */
+public class User {
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+}
